d26baf66640f7e6fe579f305003c63a1fcda5cd9
sticklearn <sticklearnman@gmail.com>
2016-10-02 14:43:56 +0800

    Update MySort.c

--- MySort.c
+++ MySort.c
@@ -1,4 +1,7 @@
-#include <stdio.h>
+/* MySort.c */
+#include <stdlib.h>
+
+#include “MySort.h”
 
 /* select sort */
 void SelectSort(int *a, int len)
@@ -6,18 +9,21 @@ void SelectSort(int *a, int len)
 	int i, j;
 	int temp;
 	int small;
-	
+	/* O(n^2) */
 	for (i = 0; i < len - 1; i++) {
 		small = i;
 		for (j = i; j < len; j++) {
+			/* 
+			time(len) =  (len - 1) * (len + 2) / 2  
+			*/
 			if (a[j] < a[small]) {
 				small = j; 
 			} //if
-		} // for, j
+		} // j
 			temp = a[small];
 			a[small] = a[i];
 			a[i] = temp;
-	}// for, i
+	}// i
 }
 
 
